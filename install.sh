3008fd45eb969ca0986817cf547d5470ef09a0d6
Saad Khan <saadkhankxt@hotmail.com>
2021-12-08 18:13:58 +0500

    Fix vscode tasks.json setup

--- install.sh
+++ install.sh
@@ -13,8 +13,6 @@ olddir=~/dotfiles_old
 
 # list of files/folders to symlink in homedir
 files=".bashrc .gitconfig .gitmessage .gitignore"
-folders='vscode'
-
 
 # create dotfiles_old in homedir
 echo "Creating $olddir for backup of any existing dotfiles in ~"
@@ -22,24 +20,23 @@ mkdir -p $olddir
 echo "...done"
 
 # change to the dotfiles directory
-echo "Changing to the $dir directory"
+echo "Changing to the $dir"
 cd $dir
 echo "...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
 for file in $files; do
 	echo "Moving any existing dotfiles from ~ to $olddir"
-	mv ~/$file ~/dotfiles_old/
+	mv ~/$file $olddir/
 	echo "Creating symlink to $file in home directory."
 	ln -s $dir/$file ~/$file
 done
 
-# move any existing dotfolders in homedir to dotfiles_old directory, then create symlinks
-for folder in $folders; do
-	echo "Moving any existing dotfiles from ~ to $olddir"
-	mv ~/.$folder ~/dotfiles_old/
-	echo "Creating symlink to $folder in home directory."
-	ln -s $dir/$folder ~/.$folder
-done
+# move vscode tasks.json file to ~/.config/Code/User
+codeTaskDir=~/.config/Code/User
+echo "Moving existing tasks.json from $codeTaskDir to $olddir/vscode"
+mv $codeTaskDir/tasks.json $olddir/vscode
+echo "Moving vscode tasks.json file to $codeTaskDir"
+ln -s $dir/vscode/tasks.json $codeTaskDir/tasks.json
 
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
