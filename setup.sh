8dd27a141a2436110f8172de3e215b63d2554060
SuperKXT <saadkhankxt@hotmail.com>
2022-09-03 11:35:51 +0500

    Add npm installations to setup.sh

--- setup.sh
+++ setup.sh
@@ -10,10 +10,24 @@ curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-com
 sudo apt install direnv httpie bat tilix gcc make libssl-dev libreadline-dev zlib1g-dev libsqlite3-dev gnome-tweaks
 
 # Install nvm and node 16
-if ! command -v nvm &>/dev/null; then
-	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-	source ~/.bashrc
-	nvm install 16
-	source ~/.bashrc
-	corepack enable yarn
-fi
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+source ~/.bashrc
+
+# Install node lts
+nvm install lts/*
+
+#install global npm modules for node lts
+npm i -g nodemon node-gyp npm-check electron eslint tldr serve create-react-app pm2 jsdoc electron-icon-builder ngrok yarn yarn-check eslint-plugin-jsdoc vsce typescript @svgr/cli expo-cli ngrok ts-node dotenv-vault
+
+# Install node latest
+nvm install node
+
+#install global npm modules for node lts
+npm i -g nodemon node-gyp npm-check electron eslint tldr serve create-react-app pm2 jsdoc electron-icon-builder ngrok yarn yarn-check eslint-plugin-jsdoc vsce typescript @svgr/cli expo-cli ngrok ts-node dotenv-vault
+
+nvm alias lts/* default
+nvm use default
+
+source ~/.bashrc
+corepack enable yarn
