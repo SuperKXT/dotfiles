f527404d1ad531243fcc8eca63f6938fd920739b
SuperKXT <superkxt@outlook.com>
2022-11-30 17:55:56 +0500

    Add pm2 to NPM_PACKAGES in setup.sh

--- setup.sh
+++ setup.sh
@@ -12,7 +12,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 # Install git completion script
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
 
-NPM_PACKAGES="nodemon npm-check electron eslint tldr jsdoc ngrok eslint-plugin-jsdoc vsce typescript @svgr/cli expo-cli eas-cli ts-node dotenv-vault npkill stylelint stylelint-config-standard stylelint-config-standard-scss"
+NPM_PACKAGES="nodemon npm-check electron eslint tldr jsdoc ngrok eslint-plugin-jsdoc vsce typescript @svgr/cli expo-cli eas-cli ts-node dotenv-vault npkill stylelint stylelint-config-standard stylelint-config-standard-scss pm2"
 
 #install postman
 curl https://gist.githubusercontent.com/SanderTheDragon/1331397932abaa1d6fbbf63baed5f043/raw/postman-deb.sh | sh
