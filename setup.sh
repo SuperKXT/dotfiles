da687ac37270e09f85ff36a993f59ad69315bd6e
SuperKXT <superkxt@outlook.com>
2023-01-08 19:51:38 +0500

    Add azure data studio installation

--- setup.sh
+++ setup.sh
@@ -13,6 +13,8 @@ list_from_file() {
 	echo "$packages"
 }
 
+type -p curl >/dev/null || sudo apt install curl -y
+
 # Install apt packages
 PACKAGES="$(list_from_file ./packages.txt)"
 sudo apt install "$PACKAGES"
@@ -26,8 +28,8 @@ fi
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
 
 #install postman
-curl https://gist.githubusercontent.com/SanderTheDragon/1331397932abaa1d6fbbf63baed5f043/raw/postman-deb.sh | sh
-source ~/.bashrc
+curl https://gist.githubusercontent.com/SanderTheDragon/1331397932abaa1d6fbbf63baed5f043/raw/postman-deb.sh | sh &&
+	source ~/.bashrc
 
 # get npm packages to install
 NPM_PACKAGES="$(list_from_file ./npm-packages.txt)"
@@ -58,23 +60,28 @@ if ! command -v deno &>/dev/null; then
 	curl -fsSL https://deno.land/install.sh | sh
 fi
 
-deno completions bash >/usr/local/etc/bash_completion.d/deno.bash
-source /usr/local/etc/bash_completion.d/deno.bash
+deno completions bash >/usr/local/etc/bash_completion.d/deno.bash &&
+	source /usr/local/etc/bash_completion.d/deno.bash
 
 # install gh cli
-type -p curl >/dev/null || sudo apt install curl -y
-curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg &&
-	sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg &&
-	echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null &&
-	sudo apt update &&
-	sudo apt install gh -y
-gh extension install mislav/gh-license
-
-# install dropbox
-xdg-open https://www.dropbox.com/install?os=lnx
+if ! command -v gh &>/dev/null; then
+	curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg &&
+		sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg &&
+		echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null &&
+		sudo apt update &&
+		sudo apt install gh -y &&
+		gh extension install mislav/gh-license
+fi
 
 # install anydesk
 wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | apt-key add - &&
 	echo "deb http://deb.anydesk.com/ all main" >/etc/apt/sources.list.d/anydesk-stable.list &&
 	sudo apt update &&
 	sudo apt install anydesk
+
+# install azure data studio
+wget https://go.microsoft.com/fwlink/?linkid=2215528 -O ./aszure-data-studio.deb &&
+	sudo apt install ./azure-data-studio.deb
+
+# install dropbox
+xdg-open https://www.dropbox.com/install?os=lnx
