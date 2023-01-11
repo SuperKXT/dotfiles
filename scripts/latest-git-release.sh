b97c6568138968905bae293805ea29c180ba3323
Saad Khan <superkxt@outlook.com>
2023-01-11 11:45:17 +0500

    Update latest-git-release.sh

--- scripts/latest-git-release.sh
+++ scripts/latest-git-release.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 latest_git_release() {
-	curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
+	curl --show-progres "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
 		grep '"tag_name":' |                                             # Get tag line
 		sed -E 's/.*"([^"]+)".*/\1/'                                     # Pluck JSON value
 
