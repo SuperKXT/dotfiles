4397a017cfeec15e6571d54e2878364dc2e1cd24
SuperKXT <superkxt@outlook.com>
2023-01-14 16:17:43 +0500

    Add gtk.css setup to install-themes

--- scripts/install-themes.sh
+++ scripts/install-themes.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+#################
+#  Theme Setup  #
+#################
+
 # install orchis theme
 git clone https://github.com/vinceliuice/Orchis-theme orchis &&
 	cd orchis &&
@@ -13,6 +17,10 @@ git clone https://github.com/vinceliuice/Tela-icon-theme tela &&
 	source ./install.sh red &&
 	cd .. &&
 	rm -rf tela
+
+# add gtk terminal style config
+rm "$HOME/.config/gtk-3.0/gtk.css" &&
+	ln -s "$HOME/dotfiles/config/gtk.css" "$HOME/.config/gtk-3.0/gtk.css"
 
 # TODO Gedit theme
 
