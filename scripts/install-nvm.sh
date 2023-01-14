6df68cfe254b36d34d6e573af102b792e037b423
SuperKXT <superkxt@outlook.com>
2023-01-14 21:56:51 +0500

    Add echos to install-nvm

--- scripts/install-nvm.sh
+++ scripts/install-nvm.sh
@@ -3,8 +3,10 @@
 # shellcheck source=latest-git-release.sh
 source ~/dotfiles/scripts/latest-git-release.sh
 
-echo
-echo "Setting Up NVM"
+GREEN='\e[32m'
+NC='\e[0m'
+
+echo -e "\n${GREEN}Setting Up NVM...${NC}"
 version="$(latest_git_release "nvm-sh/nvm")" &&
 	curl --progress-bar -o- "https://raw.githubusercontent.com/nvm-sh/nvm/$version/install.sh" | bash &&
 	source ~/.bashrc
