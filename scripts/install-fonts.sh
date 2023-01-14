69d665a99ab4bc3896046f104af38e15facdd571
SuperKXT <superkxt@outlook.com>
2023-01-14 13:45:23 +0500

    Fix instal-fonts script

--- scripts/install-fonts.sh
+++ scripts/install-fonts.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
+
 # shellcheck source=latest-git-release.sh
 source latest-git-release.sh
 
 folder="$HOME/.local/share/fonts"
-mkdir -p "$folder"
 
-# install iosevka
+# Install iosevka fonts
+# Term Slab for Terminal Use
+# Slab for Normal Use
 repo="be5invis/iosevka"
 tag="$(latest_git_release $repo)"
 version="${tag:1}"
@@ -14,14 +16,14 @@ name_prefix="super-ttc-sgr-iosevka"
 if [ -n "$version" ]; then
 	wget -q --show-progress "$url_prefix/$tag/$name_prefix-fixed-slab-$version.zip" -O slab.zip &&
 		wget -q --show-progress "$url_prefix/$tag/$name_prefix-term-slab-$version.zip" -O term.zip &&
-		tar xvf slab.zip --directory "$folder" --wildcards "*.ttc" &&
-		tar xvf term.zip --directory "$folder" --wildcards "*.ttc" &&
+		unzip slab.zip -d "$folder" "*.ttc" &&
+		unzip term.zip -d "$folder" "*.ttc" &&
 		rm slab.zip term.zip
 fi
 
 # download and install fonts from dropbox
 wget -q https://www.dropbox.com/sh/w465f79zweowwug/AADBkyI1xyG4meCdGE2Oogkoa?dl=1 -O fonts.zip &&
-	tar xvf fonts.zip --directory "$folder" --wildcards "*.{otf,ttf}" &&
+	unzip fonts.zip -d "$folder" "*.{otf,ttf}" &&
 	rm fonts.zip
 
 sudo fc-cache -f
