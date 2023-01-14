820fd93b50af64edad96d9035e1945355c18f62e
SuperKXT <superkxt@outlook.com>
2023-01-14 15:40:19 +0500

    Fix latest-git-release

--- scripts/latest-git-release.sh
+++ scripts/latest-git-release.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
+# Usage `latest_git_release ":user/:repo"`
 latest_git_release() {
-	curl --show-progres "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
-		grep '"tag_name":' |                                             # Get tag line
-		sed -E 's/.*"([^"]+)".*/\1/'                                     # Pluck JSON value
+	curl --progress-bar "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
+		grep '"tag_name":' |                                                   # Get tag line
+		sed -E 's/.*"([^"]+)".*/\1/'                                           # Pluck JSON value
 
 }
