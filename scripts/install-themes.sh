5a070655cf3c407b953bf616264b6748d981683b
SuperKXT <superkxt@outlook.com>
2023-01-14 21:57:10 +0500

    Add echos to install-themes

--- scripts/install-themes.sh
+++ scripts/install-themes.sh
@@ -4,30 +4,31 @@
 #  Theme Setup  #
 #################
 
-# install orchis theme
-echo
-echo "Setting up orchis theme..."
+GREEN='\e[32m'
+NC='\e[0m'
+
+echo -e "\n${GREEN}Setting Up Orchis Theme...${NC}"
 git clone https://github.com/vinceliuice/Orchis-theme orchis &&
 	cd orchis &&
 	./install.sh -t red -c light -s compact -l --round 0px --tweaks compact primary &&
 	cd .. &&
 	rm -rf orchis
 
-# install tela icon theme
-echo
-echo "Setting up tela icon theme..."
+# TODO Install firefox orchis theme
+
+echo -e "\n${GREEN}Setting Up Tela Icon Theme...${NC}"
 git clone https://github.com/vinceliuice/Tela-icon-theme tela &&
 	cd tela &&
-	source ./install.sh red &&
+	./install.sh red &&
 	cd .. &&
 	rm -rf tela
 
-# add gtk terminal style config
-echo
-echo "Setting up gtk terminal styles..."
-rm "$HOME/.config/gtk-3.0/gtk.css" &&
+echo -e "\n${GREEN}Setting Up Gtk Terminal Styles...${NC}"
+rm -f "$HOME/.config/gtk-3.0/gtk.css" &&
 	ln -s "$HOME/dotfiles/theme/gtk.css" "$HOME/.config/gtk-3.0/gtk.css"
 
 # TODO Gedit theme
 
 # TODO Tilix theme
+
+# TODO Set theme, icons, and fonts
