9b02fa6b2d1dc3ed02d59eff9b919d8aa5e8cbb8
SuperKXT <superkxt@outlook.com>
2023-01-15 14:00:16 +0500

    Add firefox theme setup to install-themes

--- scripts/install-themes.sh
+++ scripts/install-themes.sh
@@ -11,10 +11,10 @@ echo -e "\n${GREEN}Setting Up Orchis Theme...${NC}"
 git clone https://github.com/vinceliuice/Orchis-theme orchis &&
 	cd orchis &&
 	./install.sh -t red -c light -s compact -l --round 0px --tweaks compact primary &&
+	cp -rvf ./src/firefox/chrome ~/.mozilla/firefox/*.default/ &&
+	cp -vf ./src/firefox/configuration/user.js ~/.mozilla/firefox/*.default/ &&
 	cd .. &&
 	rm -rf orchis
-
-# TODO Install firefox orchis theme
 
 echo -e "\n${GREEN}Setting Up Tela Icon Theme...${NC}"
 git clone https://github.com/vinceliuice/Tela-icon-theme tela &&
