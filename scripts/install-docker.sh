f279bd15a6548e57b92f6acde07241d040b09c7f
SuperKXT <superkxt@outlook.com>
2024-02-22 14:30:47 +0500

    Update `portainer` port in `install-docker.sh`

--- scripts/install-docker.sh
+++ scripts/install-docker.sh
@@ -15,7 +15,7 @@ if ! command -v docker &>/dev/null; then
 	sudo systemctl enable containerd.service
 	rm ./get-docker.sh
 	docker volume create portainer_data
-	docker run -d -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+	docker run -d -p 9000:9000 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
 fi
 
 # Install Portainer
