dbf97d08ac627ab5b5e94722503dd6a58272bade
SuperKXT <superkxt@outlook.com>
2024-02-22 14:31:49 +0500

    Tweak `install-docker.sh`

--- scripts/install-docker.sh
+++ scripts/install-docker.sh
@@ -17,5 +17,3 @@ if ! command -v docker &>/dev/null; then
 	docker volume create portainer_data
 	docker run -d -p 9000:9000 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
 fi
-
-# Install Portainer
