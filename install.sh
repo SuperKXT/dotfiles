a04f665beddf927a99997e20519d9c409bf10ef9
SuperKXT <superkxt@outlook.com>
2024-07-06 17:56:36 +0500

    Fix `ProtonVPN` installation
    Co-authored-by: MBM1607 <muhammadkhan1607@gmail.com>

--- install.sh
+++ install.sh
@@ -162,8 +162,8 @@ fi
 # install proton vpn cli
 if ! command -v protonvpn-cli &>/dev/null; then
 	echo -e "\n${GREEN}Installing Proton VPN CLI...${NC}"
-	wget -q --show-progress https://repo.protonvpn.com/debian/dists/stable/main/binary-all/protonvpn-stable-release_1.0.3_all.deb -O proton_repo.deb
-	sudo apt -qq install -y ./proton_repo.deb
+	wget -q --show-progress https://repo.protonvpn.com/debian/dists/stable/main/binary-all/protonvpn-stable-release_1.0.3-3_all.deb -O proton_repo.deb
+	sudo dpkg i ./proton_repo.deb
 	rm ./proton_repo.deb
 	sudo apt update
 	sudo apt -qq install -p proton-vpn-gnome-desktop
