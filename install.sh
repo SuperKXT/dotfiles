51dec174ee3e85a6755124c3517945d6c59dff54
SuperKXT <superkxt@outlook.com>
2024-07-28 14:41:53 +0500

    Update proton vpn installation

--- install.sh
+++ install.sh
@@ -161,11 +161,11 @@ fi
 
 # install proton vpn
 if ! command -v protonvpn-app &>/dev/null; then
-	echo -e "\n${GREEN}Installing Proton VPN CLI...${NC}"
+	echo -e "\n${GREEN}Installing Proton VPN...${NC}"
 	wget -q --show-progress https://repo.protonvpn.com/debian/dists/stable/main/binary-all/protonvpn-stable-release_1.0.3-3_all.deb -O proton_repo.deb
 	sudo dpkg i ./proton_repo.deb
 	rm ./proton_repo.deb
-	sudo apt update
+	sudo apt -qq update
 	sudo apt -qq install -p proton-vpn-gnome-desktop
 fi
 
