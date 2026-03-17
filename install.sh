4a1f6d119d81df0b06978cc4ed1d2f029068c297
SuperKXT <superkxt@outlook.com>
2026-03-17 18:30:05 +0500

    Add cosmic applets to replace gnome shell extensions

--- install.sh
+++ install.sh
@@ -21,10 +21,10 @@ if [ "$EUID" -eq 0 ]; then
 fi
 
 # Check if curl is installed, if not install it
-type -p curl >/dev/null || sudo apt install curl -y
+type -p curl >/dev/null || sudo apt -qq install -y curl
 
 # Check if xargs is installed, if not install it
-type -p xargs >/dev/null || sudo apt install xargs -y
+type -p xargs >/dev/null || sudo apt -qq install -y xargs
 
 # Install apt packages
 echo -e "\n${GREEN}Setting Up APT Packages...${NC}"
@@ -33,7 +33,7 @@ xargs sudo apt -qq install -y <lists/apt-packages.txt
 # Install gear-lever (AppImage manager)
 if ! flatpak info it.mijorus.gearlever &>/dev/null 2>&1; then
 	echo -e "\n${GREEN}Installing gear-lever...${NC}"
-	flatpak install -y flathub it.mijorus.gearlever
+	flatpak install -y --noninteractive flathub it.mijorus.gearlever &>/dev/null
 fi
 
 # Install or update nvm
@@ -46,19 +46,19 @@ export NVM_DIR="$HOME/.nvm"
 # Install nvm node versions
 for version in lts/*; do
 	echo -e "\n${GREEN}Setting Up Node Version: $version...${NC}"
-	nvm install "$version"
-	xargs npm install -g <lists/npm-packages.txt
-	corepack enable
-	corepack prepare yarn@stable --activate
-	corepack prepare pnpm@latest --activate
+	nvm install "$version" &>/dev/null
+	xargs npm install -g --silent <lists/npm-packages.txt
+	corepack enable &>/dev/null
+	corepack prepare yarn@stable --activate &>/dev/null
+	corepack prepare pnpm@latest --activate &>/dev/null
 done
-nvm alias lts/* default
-nvm use default
+nvm alias lts/* default &>/dev/null
+nvm use default &>/dev/null
 
 # Install Deno
 if ! command -v deno &>/dev/null; then
 	echo -e "\n${GREEN}Installing Deno...${NC}"
-	curl -fsSL https://deno.land/x/install/install.sh | sh
+	curl -fsSL https://deno.land/x/install/install.sh | sh &>/dev/null
 	export DENO_INSTALL="/home/superkxt/.deno"
 	export PATH="$DENO_INSTALL/bin:$PATH"
 fi
@@ -66,12 +66,12 @@ fi
 # install gh cli
 if ! command -v gh &>/dev/null; then
 	echo -e "\n${GREEN}Installing GitHub CLI...${NC}"
-	curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+	curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg status=none
 	sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
 	echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null
-	sudo apt -qq update
+	sudo apt -qq update &>/dev/null
 	sudo apt -qq install -y gh
-	gh extension install mislav/gh-license
+	gh extension install mislav/gh-license &>/dev/null
 	gh auth login -w -s admin:public_key
 fi
 
@@ -79,29 +79,29 @@ fi
 if ! command -v code &>/dev/null; then
 	echo -e "\n${GREEN}Installing VS Code...${NC}"
 	sudo apt -qq install -y wget gpg
-	wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor >packages.microsoft.gpg
+	wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor --batch >packages.microsoft.gpg
 	sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
 	sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
 	rm -f packages.microsoft.gpg
 	sudo apt -qq install -y apt-transport-https
-	sudo apt -qq update
+	sudo apt -qq update &>/dev/null
 	sudo apt -qq install -y code
 fi
 
 # install remmina
 if ! command -v remmina &>/dev/null; then
 	echo -e "\n${GREEN}Installing Remmina...${NC}"
-	sudo apt-add-repository ppa:remmina-ppa-team/remmina-next
-	sudo apt update
-	sudo apt install remmina remmina-plugin-rdp remmina-plugin-secret
+	sudo apt-add-repository -y ppa:remmina-ppa-team/remmina-next &>/dev/null
+	sudo apt -qq update &>/dev/null
+	sudo apt -qq install -y remmina remmina-plugin-rdp remmina-plugin-secret
 fi
 
 # install obs studio
 if ! command -v obs &>/dev/null; then
 	echo -e "\n${GREEN}Installing OBS Studio...${NC}"
-	sudo add-apt-repository -y ppa:obsproject/obs-studio
-	sudo apt update
-	sudo apt install ffmpeg obs-studio
+	sudo add-apt-repository -y ppa:obsproject/obs-studio &>/dev/null
+	sudo apt -qq update &>/dev/null
+	sudo apt -qq install -y ffmpeg obs-studio
 fi
 
 # install vivaldi
@@ -109,7 +109,7 @@ if ! command -v vivaldi &>/dev/null; then
 	echo -e "\n${GREEN}Installing Vivaldi...${NC}"
 	curl --silent https://vivaldi.com/download/archive/?platform=linux --stderr - |
 		grep -o -m 1 https://downloads.vivaldi.com/stable/vivaldi-stable_[0-9.-]*_amd64.deb |
-		xargs wget -q --show-progress -O ./vivaldi.deb
+		xargs wget -q -O ./vivaldi.deb
 	sudo apt -qq install -y ./vivaldi.deb
 	rm ./vivaldi.deb
 fi
@@ -117,7 +117,7 @@ fi
 # install Chrome
 if ! command -v google-chrome &>/dev/null; then
 	echo -e "\n${GREEN}Installing Google Chrome...${NC}"
-	wget -q --show-progress https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O ./chrome.deb
+	wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O ./chrome.deb
 	sudo apt -qq install -y ./chrome.deb
 	rm ./chrome.deb
 fi
@@ -129,9 +129,9 @@ if ! command -v anydesk &>/dev/null; then
 	echo
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
 		echo -e "\n${GREEN}Installing AnyDesk...${NC}"
-		curl -fsSL https://keys.anydesk.com/repos/DEB-GPG-KEY | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/anydesk.gpg
-		echo "deb http://deb.anydesk.com/ all main" | sudo tee /etc/apt/sources.list.d/anydesk-stable.list
-		sudo apt -qq update
+		curl -fsSL https://keys.anydesk.com/repos/DEB-GPG-KEY | sudo gpg --dearmor --batch -o /etc/apt/trusted.gpg.d/anydesk.gpg
+		echo "deb http://deb.anydesk.com/ all main" | sudo tee /etc/apt/sources.list.d/anydesk-stable.list >/dev/null
+		sudo apt -qq update &>/dev/null
 		sudo apt -qq install -y anydesk
 	fi
 fi
@@ -147,7 +147,7 @@ if ! command -v rustdesk &>/dev/null; then
 		tag="$(latest_git_release "$repo")"
 		echo "Latest tag: ${tag}"
 		version="${tag:1}"
-		wget -q --show-progress "https://github.com/${repo}/releases/download/${tag}/rustdesk-${tag}-x86_64.deb" -O rustdesk.deb
+		wget -q "https://github.com/${repo}/releases/download/${tag}/rustdesk-${tag}-x86_64.deb" -O rustdesk.deb
 		sudo apt -qq install -y ./rustdesk.deb
 		rm ./rustdesk.deb
 	fi
@@ -156,17 +156,17 @@ fi
 # install proton vpn
 if ! command -v protonvpn-app &>/dev/null; then
 	echo -e "\n${GREEN}Installing Proton VPN...${NC}"
-	wget -q --show-progress https://repo.protonvpn.com/debian/dists/stable/main/binary-all/protonvpn-stable-release_1.0.8_all.deb -O proton_repo.deb
-	sudo dpkg -i ./proton_repo.deb
+	wget -q https://repo.protonvpn.com/debian/dists/stable/main/binary-all/protonvpn-stable-release_1.0.8_all.deb -O proton_repo.deb
+	sudo dpkg -i ./proton_repo.deb &>/dev/null
 	rm ./proton_repo.deb
-	sudo apt -qq update
-	sudo apt -qq install proton-vpn-gnome-desktop
+	sudo apt -qq update &>/dev/null
+	sudo apt -qq install -y proton-vpn-gnome-desktop
 fi
 
 # Install Dropbox
 if ! command -v dropbox &>/dev/null; then
 	echo -e "\n${GREEN}Installing Dropbox...${NC}"
-	wget -q --show-progress "https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2026.01.15_amd64.deb" -O dropbox.deb
+	wget -q "https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2026.01.15_amd64.deb" -O dropbox.deb
 	sudo apt -qq install -y ./dropbox.deb
 	rm ./dropbox.deb
 fi
@@ -178,7 +178,7 @@ if ! command -v koodo-reader &>/dev/null; then
 	tag="$(latest_git_release "$repo")"
 	echo "Latest tag: ${tag}"
 	version="${tag:1}"
-	wget -q --show-progress "https://github.com/${repo}/releases/download/${tag}/Koodo-Reader-${version}-amd64.deb" -O koodo.deb
+	wget -q "https://github.com/${repo}/releases/download/${tag}/Koodo-Reader-${version}-amd64.deb" -O koodo.deb
 	sudo apt -qq install -y ./koodo.deb
 	rm ./koodo.deb
 fi
@@ -187,16 +187,16 @@ fi
 if ! command -v ngrok &>/dev/nulll; then
 	echo -e "\n${GREEN}Installing ngrok...${NC}"
 	curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null
-	echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list
-	sudo apt -qq update
+	echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list >/dev/null
+	sudo apt -qq update &>/dev/null
 	sudo apt -qq install -y ngrok
 fi
 
 # intall qBitTorrent
 if ! command -v qbittorrent &>/dev/nulll; then
 	echo -e "\n${GREEN}Installing qBitTorrent...${NC}"
-	sudo add-apt-repository -y ppa:qbittorrent-team/qbittorrent-stable
-	sudo apt -qq update
+	sudo add-apt-repository -y ppa:qbittorrent-team/qbittorrent-stable &>/dev/null
+	sudo apt -qq update &>/dev/null
 	sudo apt -qq install -y qbittorrent
 fi
 
@@ -204,9 +204,9 @@ fi
 if ! command -v spotify-client &>/dev/null; then
 	echo
 	echo -e "\n${GREEN}Installing Spotify...${NC}"
-	curl -sS https://download.spotify.com/debian/pubkey_5384CE82BA52C83A.asc | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
-	echo "deb https://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-	sudo apt -qq update
+	curl -sS https://download.spotify.com/debian/pubkey_5384CE82BA52C83A.asc | sudo gpg --dearmor --batch --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+	echo "deb https://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list >/dev/null
+	sudo apt -qq update &>/dev/null
 	sudo apt -qq install -y spotify-client
 fi
 
@@ -214,8 +214,8 @@ fi
 # install onefetch
 if ! command -v onefetch &>/dev/null; then
 	echo -e "\n${GREEN}Installing onefetch...${NC}"
-	sudo add-apt-repository -y ppa:o2sh/onefetch
-	sudo apt -qq update
+	sudo add-apt-repository -y ppa:o2sh/onefetch &>/dev/null
+	sudo apt -qq update &>/dev/null
 	sudo apt -qq install -y onefetch
 fi
 
@@ -223,12 +223,12 @@ fi
 if ! command -v wine &>/dev/null; then
 	echo
 	echo -e "\n${GREEN}Installing Wine...${NC}"
-	sudo dpkg --add-architecture i386
+	sudo dpkg --add-architecture i386 &>/dev/null
 	sudo mkdir -pm755 /etc/apt/keyrings
-	sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
-	sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/jammy/winehq-jammy.sources
-	sudo apt -qq update
-	sudo apt -qq install --install-recommends winehq-stable
+	sudo wget -q -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
+	sudo wget -q -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/jammy/winehq-jammy.sources
+	sudo apt -qq update &>/dev/null
+	sudo apt -qq install -y --install-recommends winehq-stable
 fi
 
 # install MS Teams
@@ -239,13 +239,43 @@ if ! command -v teams-for-linux &>/dev/null; then
 	tag="$(latest_git_release "$repo")"
 	echo "Latest tag: ${tag}"
 	version="${tag:1}"
-	wget -q --show-progress "https://github.com/${repo}/releases/download/${tag}/teams-for-linux_${version}_amd64.deb" -O teams.deb
+	wget -q "https://github.com/${repo}/releases/download/${tag}/teams-for-linux_${version}_amd64.deb" -O teams.deb
 	sudo apt -qq install -y ./teams.deb
 	rm ./teams.deb
 fi
 
+# Install Drives Cosmic Applet
+if ! flatpak info dev.cappsy.CosmicExtAppletDrives &>/dev/null; then
+	echo -e "\n${GREEN}Installing Drives Cosmic Applet...${NC}"
+	flatpak remote-add --if-not-exists --user cosmic https://apt.pop-os.org/cosmic/cosmic.flatpakrepo &>/dev/null
+	flatpak install -y --noninteractive dev.cappsy.CosmicExtAppletDrives &>/dev/null
+fi
+
+# Install Clipboard Manager (Flatpak version broken due to Wayland data control sandboxing)
+# TODO: Replace with Flatpak once sandboxing is fixed: https://github.com/cosmic-utils/clipboard-manager/issues/171
+# if ! flatpak info io.github.cosmic_utils.cosmic-ext-applet-clipboard-manager &>/dev/null; then
+# 	echo -e "\n${GREEN}Installing Clipboard Manager Cosmic Applet...${NC}"
+# 	flatpak install -y --noninteractive io.github.cosmic_utils.cosmic-ext-applet-clipboard-manager &>/dev/null
+# fi
+if ! command -v cosmic-ext-applet-clipboard-manager &>/dev/null; then
+	echo -e "\n${GREEN}Installing Clipboard Manager Cosmic Applet...${NC}"
+	git clone -q https://github.com/cosmic-utils/clipboard-manager /tmp/clipboard-manager
+	(
+		cd /tmp/clipboard-manager || exit
+		just build-release &>/dev/null && sudo just install &>/dev/null
+	)
+	rm -rf /tmp/clipboard-manager
+fi
+
+# Install Minimon Cosmic Applet
+if ! flatpak info io.github.cosmic_utils.minimon-applet &>/dev/null; then
+	echo -e "\n${GREEN}Installing Minimon Cosmic Applet...${NC}"
+	flatpak remote-add --if-not-exists --user cosmic https://apt.pop-os.org/cosmic/cosmic.flatpakrepo &>/dev/null
+	flatpak install -y --noninteractive io.github.cosmic_utils.minimon-applet &>/dev/null
+fi
+
 # Install Turso CLI
-curl -sSfL https://get.tur.so/install.sh | bash
+curl -sSfL https://get.tur.so/install.sh | bash &>/dev/null
 
 # Setup React Native Dev Environment
 sudo chmod u+x scripts/setup-react-native.sh
