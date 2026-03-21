f1eeced7381eb37b6d3903e3289317e2ee60337c
SuperKXT <superkxt@outlook.com>
2026-03-21 13:46:48 +0500

    Update cosmic config

---
+++ scripts/copy-cosmic-config.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+cp -rT ~/.config/cosmic/ ~/dotfiles/config/cosmic/
+rm -rf ~/dotfiles/config/cosmic/com.system76.CosmicTheme.Light
+rm -rf ~/dotfiles/config/cosmic/com.system76.CosmicTheme.Dark
+rm -rf ~/dotfiles/config/cosmic/com.system76.CosmicPortal
+rm -rf ~/dotfiles/config/cosmic/com.system76.CosmicStore
+rm -rf ~/dotfiles/config/cosmic/com.system76.CosmicPlayer
+rm -rf ~/dotfiles/config/cosmic/com.system76.CosmicAppList
+rm -rf ~/dotfiles/config/cosmic/com.system76.CosmicSettingsDaemon
+rm -f  ~/dotfiles/config/cosmic/com.system76.CosmicSettings.Wallpaper/v1/recent-folders
